5a194d4de4acf2ff209114aabde81a8057e5a517
Jan-To <jan-to@gmx.de>
2024-09-04 16:07:06 +0200

    boilerplating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    import {data} from "./store";
+
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style>
+    
+</style>
